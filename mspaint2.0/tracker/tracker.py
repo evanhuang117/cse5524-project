6aadcab8db2d065b270f5aa9bcbe0a38a66a6056
EvanHuang-executor <evanhuang117@gmail.com>
2022-04-17 17:19:23 -0400

    use cv2 to process indiv frame instead of loading entire vid into mem

--- mspaint2.0/tracker/tracker.py
+++ mspaint2.0/tracker/tracker.py
@@ -11,12 +11,13 @@ import math
 
 
 class Tracker:
-    def __init__(self, input_video, threshold=0.05, region_size=9):
+    def __init__(self, input_video, threshold=0.05, region_size=9, rescale=0.5):
         self.input = input_video
-        self.curr_frame = 0
         self.region_size = region_size
+        self.rescale = rescale
+        self.update_frames()
         self.feature_points = self.gen_feature_points(threshold)
-        plt.imshow(self.input[0], cmap='gray')
+        plt.imshow(self.curr_frame, cmap='gray')
         plt.scatter(self.feature_points[:, 1],
                     self.feature_points[:, 0], s=1, c='r')
         plt.show()
@@ -28,11 +29,13 @@ class Tracker:
         return self.next()
 
     def next(self):
-        if self.curr_frame < len(self.input) - 2:
-            self.curr_frame += 1
-            curr = self.input[self.curr_frame]
-            next = self.input[self.curr_frame + 1]
+        if self.update_frames():
+            curr = self.curr_frame
+            next = self.next_frame
+
             # 4d vectors : r, c, u, v
+            # flows = self.feature_points.copy()
+            # flows = np.hstack((flows, np.zeros((self.feature_points.shape[0], 2))))
             flows = np.zeros((self.feature_points.shape[0], 4))
             # go through all feature points we identified and calculate klt for each
             for i, (r, c) in enumerate(self.feature_points):
@@ -46,15 +49,36 @@ class Tracker:
                     # flow is none if we can't invert a mat
                     if flow_res:
                         f, mag = flow_res
-                        if np.linalg.norm(f) < np.linalg.norm(mag):
-                            # add the flow vector to the prev r,c to find the updated pos.
-                            new_point = (r+f[0], c+f[1])
-                            if new_point[0] < curr.shape[0] and new_point[1] < curr.shape[1]:
-                                # add updated r, c and flow vector to array of all flows
-                                flows[i] = np.insert(f, 0, [r, c])
-                                self.feature_points[i] = new_point
+                        # add the flow vector to the prev r,c to find the updated pos.
+                        new_point = (int(np.ceil(r+f[0])),
+                                     int(np.ceil(c+f[1])))
+                        if new_point[0] < curr.shape[0] and new_point[0] >= 0 \
+                                and new_point[1] < curr.shape[1] and new_point[1] >= 0:
+                            # add updated r, c and flow vector to array of all flows
+                            flows[i] = np.array([r, c, f[0], f[1]])
+                            self.feature_points[i] = new_point
             return flows
         raise StopIteration()
+
+    def update_frames(self):
+        # for some reason uint8 breaks everything so need to divide
+        _, self.curr_frame = self.input.read()
+        self.curr_frame = cv2.cvtColor(
+            self.curr_frame, cv2.COLOR_BGR2GRAY) / 255
+         # rescale the image to make it easier to detect smaller movements
+        width = int(self.curr_frame.shape[1] * self.rescale)
+        height = int(self.curr_frame.shape[0] * self.rescale)
+        dim = (width, height)
+
+        self.curr_frame = cv2.resize(
+            self.curr_frame, dim, interpolation=cv2.INTER_AREA)
+
+        successful_read, self.next_frame = self.input.read()
+        self.next_frame = cv2.cvtColor(
+            self.next_frame, cv2.COLOR_BGR2GRAY) / 255
+        self.next_frame = cv2.resize(
+            self.next_frame, dim, interpolation=cv2.INTER_AREA)
+        return successful_read
 
     """
     find features to track based off first 2 frames using mei
@@ -62,7 +86,7 @@ class Tracker:
     """
 
     def gen_feature_points(self, threshold):
-        mei = np.abs(self.input[1] - self.input[0])
+        mei = np.abs(self.next_frame - self.curr_frame)
         mei = mei > threshold
         mei = scipy.ndimage.binary_opening(mei)
         mei = scipy.ndimage.binary_closing(mei)
@@ -116,7 +140,3 @@ class Tracker:
             return (np.linalg.inv(a.T @ a) @ a.T @ ft).reshape(2), ft
         except:
             return None
-#video = np.squeeze(skvideo.utils.rgb2gray(skvideo.io.vread('C:\\Users\\Jaiydev Gupta\\Documents\\5524 project\\cse5524-project\\data\\up_move_right.mp4'))/255)
-#video = np.squeeze(skvideo.io.vread('C:\Users\Jaiydev Gupta\Documents\5524 project\cse5524-project\data')/255);
-#print(video.shape)
-#Tracker(video, threshold=0.1, region_size=15)
