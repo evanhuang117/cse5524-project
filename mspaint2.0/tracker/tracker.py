a32d5fbd93c52324b4b20cb796fb964c5370ddcc
EvanHuang-executor <evanhuang117@gmail.com>
2022-04-18 13:57:07 -0400

    add tracking given a region+region size instead of interest points

--- mspaint2.0/tracker/tracker.py
+++ mspaint2.0/tracker/tracker.py
@@ -7,13 +7,16 @@ import math
 
 
 class Tracker:
-    def __init__(self, input_video, threshold=0.05, region_size=9, scale=0.5):
+    def __init__(self, input_video, threshold=0.05, region_size=9, scale=0.5, regions=None):
         self.input = input_video
         self.region_size = region_size
         self.scale = scale
         self.curr_frame = None
+        self.regions = regions
+        # read in the first 2 frames
         self.update_frames()
-        self.feature_points = self.gen_feature_points(threshold)
+        if regions is None:
+            self.feature_points = self.gen_feature_points(threshold)
         plt.imshow(self.curr_frame_scaled, cmap='gray')
         plt.scatter(self.feature_points[:, 1],
                     self.feature_points[:, 0], s=1, c='r')
@@ -27,42 +30,90 @@ class Tracker:
 
     def next(self):
         if self.update_frames():
-            curr = self.curr_frame_scaled
-            next = self.next_frame_scaled
-
-            # 4d vectors : r, c, u, v
-            # flows = self.feature_points.copy()
-            # flows = np.hstack((flows, np.zeros((self.feature_points.shape[0], 2))))
-            flows = np.zeros((self.feature_points.shape[0], 4))
-            # go through all feature points we identified and calculate klt for each
-            for i, (r, c) in enumerate(self.feature_points):
-                # find region centered around feature point
-                offset = math.floor(self.region_size/2)
-                region0 = curr[r-offset:r+offset+1, c-offset:c+offset+1]
-                region1 = next[r-offset:r+offset+1, c-offset:c+offset+1]
-                # filter out border of region_size/2 around the image
-                if region0.size >= self.region_size ** 2 and region1.size >= self.region_size ** 2:
-                    flow_res = self.flow2(region0, region1)
-                    # flow is none if we can't invert a mat
-                    if flow_res:
-                        f, mag = flow_res
-                        f_upscale = f / self.scale
-                        # scale the r, c  up to the original size
-                        r_upscale = r / self.scale
-                        c_upscale = c / self.scale
-                        # add the flow vector to the prev r,c to find the updated pos.
-                        new_point = (int(np.ceil(r+f[0])),
-                                     int(np.ceil(c+f[1])))
-                        new_point_upscale = (int(np.ceil(r_upscale+f_upscale[0])),
-                                             int(np.ceil(c_upscale+f_upscale[1])))
-                        if new_point[0] < curr.shape[0] and new_point[0] >= 0 \
-                                and new_point[1] < curr.shape[1] and new_point[1] >= 0:
-                            # add updated r, c and flow vector to array of all flows
-                            flows[i] = np.array(
-                                [r_upscale, c_upscale, f_upscale[0], f_upscale[1]])
-                            self.feature_points[i] = new_point
-            return flows
+            if self.regions is None:
+                return self.track_points()
+            else:
+                return self.track_region()
         raise StopIteration()
+
+    def track_points(self):
+        """
+            tracks interest points and the area around them (self.region_size)
+            instead of an entire region
+        """
+        curr = self.curr_frame_scaled
+        next = self.next_frame_scaled
+
+        # 4d vectors : r, c, u, v
+        # flows = self.feature_points.copy()
+        # flows = np.hstack((flows, np.zeros((self.feature_points.shape[0], 2))))
+        flows = np.zeros((self.feature_points.shape[0], 4))
+        # go through all feature points we identified and calculate klt for each
+        for i, (r, c) in enumerate(self.feature_points):
+            # find region centered around feature point
+            offset = math.floor(self.region_size/2)
+            region0 = curr[r-offset:r+offset+1, c-offset:c+offset+1]
+            region1 = next[r-offset:r+offset+1, c-offset:c+offset+1]
+            # filter out border of region_size/2 around the image
+            if region0.size >= self.region_size ** 2 and region1.size >= self.region_size ** 2:
+                flow_res = self.flow(region0, region1)
+                # flow is none if we can't invert a mat
+                if flow_res:
+                    f, mag = flow_res
+                    f_upscale = f / self.scale
+                    # scale the r, c  up to the original size
+                    r_upscale = r / self.scale
+                    c_upscale = c / self.scale
+                    # add the flow vector to the prev r,c to find the updated pos.
+                    new_point = (int(np.ceil(r+f[0])),
+                                 int(np.ceil(c+f[1])))
+                    new_point_upscale = (int(np.ceil(r_upscale+f_upscale[0])),
+                                         int(np.ceil(c_upscale+f_upscale[1])))
+                    # make sure new points are within bounds of image
+                    if new_point[0] < curr.shape[0] and new_point[0] >= 0 \
+                            and new_point[1] < curr.shape[1] and new_point[1] >= 0:
+                        # add updated r, c and flow vector to array of all flows
+                        flows[i] = np.array(
+                            [r_upscale, c_upscale, f_upscale[0], f_upscale[1]])
+                        self.feature_points[i] = new_point
+        return flows
+
+    def track_region(self):
+        """
+            run KLT tracking on a region in a scaled down version of the video then 
+            upscale it to the original image size
+
+        """
+        curr = self.curr_frame_scaled
+        next = self.next_frame_scaled
+        flows = np.zeros((self.regions.shape[0], 4))
+        for i, (r, c, win_size) in enumerate(self.regions):
+            offset = int(math.floor(win_size/2) + win_size / 2)
+            region0 = curr[r-offset:r+offset+1, c-offset:c+offset+1]
+            region1 = next[r-offset:r+offset+1, c-offset:c+offset+1]
+            if region0.size >= win_size ** 2 and region1.size >= win_size ** 2:
+                flow_res = self.flow(region0, region1)
+                # flow is none if we can't invert a mat
+                if flow_res:
+                    f, mag = flow_res
+                    f_upscale = f / self.scale
+                    # scale the r, c  up to the original size
+                    r_upscale = r / self.scale
+                    c_upscale = c / self.scale
+                    # add the flow vector to the prev r,c to find the updated pos.
+                    new_point = (int(np.ceil(r+f[0])),
+                                 int(np.ceil(c+f[1])))
+                    new_point_upscale = (int(np.ceil(r_upscale+f_upscale[0])),
+                                         int(np.ceil(c_upscale+f_upscale[1])))
+                    if new_point[0] < curr.shape[0] and new_point[0] >= 0 \
+                            and new_point[1] < curr.shape[1] and new_point[1] >= 0:
+                        # add current r, c and flow vector to array of all flows
+                        # so flows are "old" pos with the corresponding flow from that point
+                        flows[i] = np.array(
+                            [r_upscale, c_upscale, f_upscale[0], f_upscale[1]])
+                        # update pos of region
+                        self.regions[i] = (*new_point, win_size)
+            return flows
 
     def update_frames(self):
         """
@@ -122,25 +173,6 @@ class Tracker:
         return np.array(mei_filtered)
 
     def flow(self, patch0, patch1):
-        patch0 = patch0
-        patch1 = patch1
-        Gx = np.array([[-1, 0, 1],
-                       [-2, 0, 2],
-                       [-1, 0, 1]]) / 8
-        Gy = np.array([[-1, -2, -1],
-                       [0, 0, 0],
-                       [1, 2, 1]]) / 8
-
-        fx = cv2.filter2D(patch1.astype(np.float32), -1, Gx)
-        fy = cv2.filter2D(patch1.astype(np.float32), -1, Gy)
-        denom = np.sqrt(fx**2+fy**2)
-        with np.errstate(divide='ignore', invalid='ignore'):
-            mag = -(patch1-patch0) / denom
-            u = fx/denom * mag
-            v = fy/denom * mag
-        return (u, v, mag)
-
-    def flow2(self, patch0, patch1):
         # patch0 = rgb2gray(patch0)
         # patch1 = rgb2gray(patch1)
         Gx = np.array([[-1, 0, 1],
